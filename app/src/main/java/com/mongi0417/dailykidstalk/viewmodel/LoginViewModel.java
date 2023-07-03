947287b7ef9114bc2e263fffc437f129b39c610f
Hub0128 <bsh4128@naver.com>
2023-07-03 17:43:54 +0900

    change canLogin and loginUser Method

--- app/src/main/java/com/mongi0417/dailykidstalk/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/mongi0417/dailykidstalk/viewmodel/LoginViewModel.java
@@ -6,20 +6,17 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginViewModel extends ViewModel {
     private boolean hasAccount = false;
-    private String email, password;
 
     public LoginViewModel() {
     }
 
     public boolean canLogin(String email, String password) {
-        this.email = email;
-        this.password = password;
-        loginUser();
-        return hasAccount;
+        return loginUser(email, password);
     }
 
-    private void loginUser() {
+    private boolean loginUser(String email, String password) {
         FirebaseAuth auth = FirebaseAuth.getInstance();
         auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> hasAccount = task.isSuccessful());
+        return hasAccount;
     }
 }
