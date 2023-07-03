c20d268ce4bab8b8b82b4845b7000905aff3065e
Hub0128 <bsh4128@naver.com>
2023-07-03 17:31:32 +0900

    add home activity

--- app/src/main/java/com/mongi0417/dailykidstalk/view/HomeActivity.java
+++ app/src/main/java/com/mongi0417/dailykidstalk/view/HomeActivity.java
@@ -1,6 +1,19 @@
 package com.mongi0417.dailykidstalk.view;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mongi0417.dailykidstalk.databinding.LayoutHomeBinding;
+
 public class HomeActivity extends AppCompatActivity {
+    LayoutHomeBinding homeBinding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        homeBinding = LayoutHomeBinding.inflate(getLayoutInflater());
+        setContentView(homeBinding.getRoot());
+    }
 }
