ca7d521c17a35764779a78b241a8b740bda6a455
Hub0128 <bsh4128@naver.com>
2023-07-07 22:37:43 +0900

    add sign up view model

---
+++ app/src/main/java/com/mongi0417/dailykidstalk/viewmodel/SignUpViewModel.java
@@ -0,0 +1,21 @@
+package com.mongi0417.dailykidstalk.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class SignUpViewModel extends ViewModel {
+    private boolean hasDuplicateEmail = false;
+    private boolean hasDuplicateNickname = false;
+
+    public SignUpViewModel() {
+    }
+
+    public boolean hasDuplicateEmail() {
+
+        return hasDuplicateEmail;
+    }
+
+    public boolean hasDuplicateNickname() {
+
+        return hasDuplicateNickname;
+    }
+}
