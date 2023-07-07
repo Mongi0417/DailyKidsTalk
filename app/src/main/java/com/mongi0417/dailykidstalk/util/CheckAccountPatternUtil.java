5059475cbe6329da38b10b70a73047e3a80c85be
Hub0128 <bsh4128@naver.com>
2023-07-08 02:16:34 +0900

    add util for check account pattern

---
+++ app/src/main/java/com/mongi0417/dailykidstalk/util/CheckAccountPatternUtil.java
@@ -0,0 +1,19 @@
+package com.mongi0417.dailykidstalk.util;
+
+public class CheckAccountPatternUtil {
+    public static boolean isValidEmail(String email) {
+        return false;
+    }
+
+    public static boolean isValidNickname(String nickname) {
+        return false;
+    }
+
+    public static boolean isValidPassword(String password) {
+        return false;
+    }
+
+    public static boolean isPasswordCheckSuccess(String password1, String password2) {
+        return false;
+    }
+}
