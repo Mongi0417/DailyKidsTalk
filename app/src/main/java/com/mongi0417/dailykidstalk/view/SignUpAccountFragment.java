55953cf17fbb0edc2e344254162e43c44d23d8d1
Hub0128 <bsh4128@naver.com>
2023-07-07 22:36:31 +0900

    rename fragment

---
+++ app/src/main/java/com/mongi0417/dailykidstalk/view/SignUpAccountFragment.java
@@ -0,0 +1,53 @@
+package com.mongi0417.dailykidstalk.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.mongi0417.dailykidstalk.databinding.FragmentSignUpAccountBinding;
+import com.mongi0417.dailykidstalk.viewmodel.SignUpViewModel;
+
+public class SignUpAccountFragment extends Fragment implements View.OnClickListener {
+    private FragmentSignUpAccountBinding signUpAccountBinding;
+    private SignUpViewModel signUpViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        signUpAccountBinding = FragmentSignUpAccountBinding.inflate(inflater, container, false);
+
+        signUpViewModel = new ViewModelProvider(this).get(SignUpViewModel.class);
+
+        return signUpAccountBinding.getRoot();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == signUpAccountBinding.signUpBtnCheckEmail.getId()) { // 이메일 중복 검사
+            if(signUpViewModel.hasDuplicateEmail())
+                Toast.makeText(getActivity(), "중복된 이메일이 존재합니다.", Toast.LENGTH_SHORT).show();
+            else {
+
+            }
+        } else if (view.getId() == signUpAccountBinding.signUpBtnCheckNickname.getId()) { // 닉네임 중복 검사
+            if(signUpViewModel.hasDuplicateNickname())
+                Toast.makeText(getActivity(), "중복된 닉네임이 존재합니다.", Toast.LENGTH_SHORT).show();
+            else {
+
+            }
+        } else { // 다음 버튼
+
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        signUpAccountBinding = null; // View Binding 사용 시, View보다 오래 지속되기 때문에 메모리 누수가 발생할 수 있음. 이를 방지.
+    }
+}
--- app/src/main/java/com/mongi0417/dailykidstalk/view/SignUpFragment.java
+++
@@ -1,7 +0,0 @@
-package com.mongi0417.dailykidstalk.view;
-
-import androidx.fragment.app.Fragment;
-
-public class SignUpFragment extends Fragment {
-
-}
