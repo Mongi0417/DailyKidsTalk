a329b644513daa91ef363d9b0e7885cc4ef3a3dc
Hub0128 <bsh4128@naver.com>
2023-07-07 22:37:06 +0900

    add setContentView

--- app/src/main/java/com/mongi0417/dailykidstalk/view/SignUpActivity.java
+++ app/src/main/java/com/mongi0417/dailykidstalk/view/SignUpActivity.java
@@ -1,7 +1,18 @@
 package com.mongi0417.dailykidstalk.view;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.mongi0417.dailykidstalk.databinding.LayoutSignUpBinding;
 
+public class SignUpActivity extends AppCompatActivity {
+    private LayoutSignUpBinding signUpBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        signUpBinding = LayoutSignUpBinding.inflate(getLayoutInflater());
+        setContentView(signUpBinding.getRoot());
+    }
 }
